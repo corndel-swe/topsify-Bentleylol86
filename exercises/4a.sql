151b5706f233d0f5a2fada9c3879eafd3117d52d
Bentleylol86 <Laurence.Woodhouse@bentley.co.uk>
2025-01-06 16:31:17 +0000

    the struggle continues

--- exercises/4a.sql
+++ exercises/4a.sql
@@ -1,3 +1,8 @@
 -- List the name of every album in the database
 -- Include the artist name
 -- (Make sure to select the album name first, then the artist name)
+SELECT name 
+FROM album
+AND name 
+FROM artist
+ ;
