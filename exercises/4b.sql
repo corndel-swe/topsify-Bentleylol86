ca73367c501c83481a0e5f59b5b1bed9c23ec73d
Bentleylol86 <Laurence.Woodhouse@bentley.co.uk>
2025-01-07 12:30:17 +0000

    the struggle continues part 2

--- exercises/4b.sql
+++ exercises/4b.sql
@@ -1,3 +1,12 @@
 -- List the 10 slowest tracks in the database
 -- Order by tempo, with the slowest first
 -- Include the track id, track title and track tempo
+
+
+SELECT tracks.id, tracks.name, features.tempo
+FROM features
+INNER JOIN tracks ON features.track_id = tracks.id
+ORDER BY features.tempo ASC
+LIMIT 10;
+
+
